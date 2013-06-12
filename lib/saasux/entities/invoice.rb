7d576dfeb8b81aa7bc09c6d78c6751d2ec80470f
David Peterson <dhpeterson@gmail.com>
2013-06-12 16:08:49 +1000

    Added missing invoice fields and removed fields that were added in error.

--- lib/saasux/entities/invoice.rb
+++ lib/saasux/entities/invoice.rb
@@ -10,25 +10,29 @@ module SaasuX
     text_node :transaction_type, "transactionType", :default_value => ""
     text_node :date, "date", :default_value => ""
     text_node :contact_uid, "contactUid", :default_value => ""
+    text_node :ship_to_contact_uid, "shipToContactUid", :default_value => ""
     text_node :summary, "summary", :default_value => ""
     text_node :tags, "tags", :default_value => ""
     text_node :ccy, "ccy", :default_value => ""
     text_node :auto_populate_fx_rate, "autoPopulateFxRate", :default_value => ""
     text_node :fc_to_bc_fx_rate, "fcToBcFxRate", :default_value => ""
+    text_node :notes, "notes", :default_value => ""
+    text_node :external_notes, "externalNotes", :default_value => ""
     text_node :requires_follow_up, "requiresFollowUp", :default_value => ""
     text_node :due_or_expiry_date, "dueOrExpiryDate", :default_value => ""
     text_node :layout, "layout", :default_value => ""
     text_node :status, "status", :default_value => ""
     text_node :invoice_type, "invoiceType", :default_value => ""
     text_node :invoice_number, "invoiceNumber", :default_value => ""
+    text_node :purchase_order_number, "purchaseOrderNumber", :default_value => ""
 
     object_node :invoice_items, "invoiceItems", :class => InvoiceItems, :default_value => nil
     text_node :is_sent, "isSent", :default_value => ""
 
     object_node :trading_terms, "tradingTerms", :class => TradingTerms, :default_value => nil
 
-    text_node :total_amount_incl_tax, "totalAmountInclTax", :default_value => ""
-    text_node :total_amount_excl_tax, "totalAmountExclTax", :default_value => ""
-    text_node :total_tax_amount, "totalTaxAmount", :default_value => ""
+    # FIXME to be implemented
+    #object_node :payments, "payments", :class => InvoicePaymentItems, :default_value => nil
+    #object_node :quick_payment, "quickPayment", :class => QuickPayment, :default_value => nil
   end
 end
